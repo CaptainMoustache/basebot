6fccff6ab8f4d67a1324d1dae5ecfdd4e1dbfcaa
Jonathan Brown <8877948+CaptainMoustache@users.noreply.github.com>
2020-02-26 17:26:44 -0500

    updated presence for spring training

--- basebot.py
+++ basebot.py
@@ -19,7 +19,7 @@ class BaseballBot(discord.Client):
 	
 	async def on_ready(self):
 		print('Logged on as', self.user)
-		await self.change_presence(activity=discord.Game(name='with myself'))
+		await self.change_presence(activity=discord.Game(name='in the Grapefruit League'))
 	
 	async def on_message(self, message):
 		# don't respond to ourselves
